021d95b3e5d05b75336f4bf8eed43df1e8e91361
sps1112 <sidps1112@gmail.com>
2020-12-03 18:42:05 +0530

    Add src folder with files

---
+++ src/Application.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "Standard.h"
+#include "Vector.h"
+
+int main()
+{
+    gmath::Vector3 a(2, 2, 3);
+    gmath::Vector3 b(4, -3, 6);
+    gmath::Vector3 c = gmath::up3;
+    std::cout << (a + 3 * b - c / 0.25) << std::endl;
+    return 0;
+}
